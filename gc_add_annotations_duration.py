6cb4bbeaa2bd351ae3942b2550fb8413922b5289
cmalvr <camilaalvarez@MacBook-Pro-54.local>
2025-03-16 15:51:12 +0100

    fix

--- gc_add_annotations_duration.py
+++ gc_add_annotations_duration.py
@@ -5,9 +5,9 @@ import subprocess
 datasets = {
     "CoSeLoG": "/content/PRETSA/csv_outputs/CoSeLoG_dataset.csv",
     "Sepsis": "/content/PRETSA/csv_outputs/Sepsis_dataset.csv",
-    "Road_Traffic_Fine_Management_Process": "/content/PRETSA/csv_outputs/traffic_fines_dataset.csv"
+    "Road_Traffic_Fine_Management_Process": "/content/PRETSA/csv_outputs/Road_Traffic_Fine_Management_Process_dataset.csv"
 }
 
 for dataset, filePath in datasets.items():
     print(f"Processing: {dataset}")
-    subprocess.run(["python", "add_annotation_duration.py", dataset, filePath], check=True)
+    subprocess.run(["python", "/content/PRETSA/add_annotation_duration.py", dataset, filePath], check=True)
