35fe99a22ac0cf4b74616cd327fc0115e6d922a0
cmalvr <camilaalvarez@MacBook-Pro-54.local>
2025-03-16 21:11:00 +0100

    fix

--- startExperimentsForJournalExtension_PRETSA.py
+++ startExperimentsForJournalExtension_PRETSA.py
@@ -7,6 +7,6 @@ datasets = {
 }
 
 for dataset in datasets:
-    for k in (4, 8, 16, 32, 64):
+    for k in (4, 8, 16):
         t = 1.0
         subprocess.run(["python", "/content/PRETSA/runExperimentForJournalExtension_pretsa.py", dataset, str(k), str(t)], check=True)
