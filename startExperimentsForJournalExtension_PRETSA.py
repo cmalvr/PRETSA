c59a72ec9ade8f3459370b0dd8e770d9f38947e4
cmalvr <camilaalvarez@MacBook-Pro-54.local>
2025-03-16 18:45:33 +0100

    fix

--- startExperimentsForJournalExtension_PRETSA.py
+++ startExperimentsForJournalExtension_PRETSA.py
@@ -6,7 +6,7 @@ datasets = {
     "Road_Traffic_Fine_Management_Process"
 }
 
-for dataset in datasets.items():
+for dataset in datasets:
     for k in (4, 8, 16, 32, 64):
         t = 1.0
         subprocess.run(["python", "runExperimentForJournalExtension_pretsa.py", dataset, str(k), str(t)], check=True)
