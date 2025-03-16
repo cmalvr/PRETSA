eccc342ee84977ca09426e77bad0264018934a04
cmalvr <camilaalvarez@MacBook-Pro-54.local>
2025-03-16 20:55:25 +0100

    revert

--- pretsa.py
+++ pretsa.py
@@ -3,10 +3,8 @@ from levenshtein import levenshtein
 import sys
 from scipy.stats import wasserstein_distance
 from scipy.stats import normaltest
-#import pandas as pd
-import cudf as pd
-#import numpy as np
-import cupy as np
+import pandas as pd
+import numpy as np
 import time
 
 class Pretsa:
